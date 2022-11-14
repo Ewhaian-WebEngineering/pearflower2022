2957c65da2a59a7890e2bfae2b59560d9af1f89b
gaga <jcky0511@gmail.com>
2022-11-15 00:25:01 +0900

    header

---
+++ src/views/WebGameHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./WebGameHeader.css"
+
+function WebGameHeader() {
+    return (
+        <div>
+            <div className="header">
+                <img className="logo" alt="logo" src="/img/logo_main.png" />
+            </div>
+        </div>
+    )
+}
+
+export default WebGameHeader
