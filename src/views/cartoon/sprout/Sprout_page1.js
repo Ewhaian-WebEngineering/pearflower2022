1014aca94350afaa77bc6820e81c05233fe9d49c
YoonKyoungMin <yoongang02@gmail.com>
2022-11-16 00:58:41 +0900

    [cartoon] sprout1 레이아웃 배치

--- src/views/cartoon/sprout/Sprout_page1.js
+++ src/views/cartoon/sprout/Sprout_page1.js
@@ -1,4 +1,78 @@
+import React from "react";
+import "../bee/Bee_page1.css";
+import "./Sprout_page1.css";
+import "../SkipBtn.css";
+
+function setScreenSize() {
+  let vh = window.innerHeight;
+  let vw = window.innerWidth;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
+}
 function Sprout_page1() {
-  return <div></div>;
+  setScreenSize();
+  return (
+    <div className="bee_container">
+      <div className="bee_background_wrap">
+        <div id="sprout_page1">
+          <div id="sprout_page1_cut1_sweat" className="inlineBlock"></div>
+          <div
+            id="sprout_page1_cut1_coffee_cloud1"
+            className="inlineBlock"
+          ></div>
+          <div
+            id="sprout_page1_cut1_coffee_cloud2"
+            className="inlineBlock"
+          ></div>
+          <div
+            id="sprout_page1_cut1_coffee_cloud3"
+            className="inlineBlock"
+          ></div>
+          <div id="sprout_page1_cut1_clock_wrap" className="inlineBlock">
+            <div id="sprout_page1_cut1_clock" className="inlineBlock"></div>
+            <div
+              id="sprout_page1_cut1_clock_long"
+              className="inlineBlock"
+            ></div>
+            <div
+              id="sprout_page1_cut1_clock_short"
+              className="inlineBlock"
+            ></div>
+          </div>
+
+          <div
+            id="sprout_page1_cut2_person1_check"
+            className="inlineBlock"
+          ></div>
+          <div id="sprout_page1_cut2_cat_talk" className="inlineBlock"></div>
+          <div
+            id="sprout_page1_cut2_person2_heart"
+            className="inlineBlock"
+          ></div>
+          <div
+            id="sprout_page1_cut2_person2_talk"
+            className="inlineBlock"
+          ></div>
+          <div
+            id="sprout_page1_cut2_person1_hair"
+            className="inlineBlock"
+          ></div>
+
+          <div id="sprout_page1_cut3_cat" className="inlineBlock"></div>
+          <div id="sprout_page1_cut3_downTree" className="inlineBlock"></div>
+          <div
+            id="sprout_page1_cut3_person_effect"
+            className="inlineBlock"
+          ></div>
+          <div id="sprout_page1_cut3_persons" className="inlineBlock"></div>
+          <div
+            id="sprout_page1_cut3_person_heart"
+            className="inlineBlock"
+          ></div>
+        </div>
+      </div>
+      <div className="skipBtn inlineBlock"></div>
+    </div>
+  );
 }
 export default Sprout_page1;
