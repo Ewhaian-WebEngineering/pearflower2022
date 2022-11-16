d3e56854cb44bf03202f2ebeb1b14076dcf04ac5
JK-cs <wskje0106@ewhain.net>
2022-11-17 02:52:31 +0900

    gamepage PC layout modified

--- src/views/GamePage.js
+++ src/views/GamePage.js
@@ -1,18 +1,14 @@
 import React, { useEffect } from 'react'
 import Header from './WebGameHeader'
 import './GamePage.css'
-// import guideText from '/img/guideText.png'
-// import window from '/img/window.png'
-// import door from '/img/lockedDoor.png'
-// import character from '/img/gameCharacter.png'
-// import key from '/img/key.png'
 
 
 function GamePage(props) {
 
     return (
         <div >
-            <div className="background">
+            <div>
+                <img src='/img/game/background.png' className="background" alt="배경" />
                 <Header />
                 <img src='/img/game/guideText.png' className="guideText" alt="설명" />
                 <img src='/img/game/window.png' className="keyWindow" />
