063c8006a48dec2f7cfa89c3ed4980ca592d6c16
JK-cs <wskje0106@ewhain.net>
2022-11-20 00:11:58 +0900

    drag & drop

--- src/views/game/GamePage.js
+++ src/views/game/GamePage.js
@@ -1,53 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Header from './WebGameHeader'
 import './GamePage.css'
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+
 
 
 
 function GamePage(props) {
-    // var img_L = 150;
-    // var img_T = 282;
-    // var targetObj;
-
-    // function getLeft(o) {
-    //     return parseInt(o.style.left.replace('px', ''));
-    // }
-
-    // function getTop(o) {
-    //     return parseInt(o.style.top.replace('px', ''));
-    // }
-
-    // function moveDrag(e) {
-    //     var e_obj = window.event ? window.event : e;
-    //     var dmvx = parseInt(e_obj.clientX + img_L);
-    //     var dmvy = parseInt(e_obj.clientY + img_T);
-    //     targetObj.style.left = dmvx + "px";
-    //     targetObj.style.top = dmvy + "px";
-    //     return false;
-    // }
-
-    // function startDrag(e, obj) {
-    //     targetObj = obj;
-    //     var e_obj = window.event ? window.event : e;
-    //     img_L = getLeft(obj) - e_obj.clientX;
-    //     img_T = getTop(obj) - e_obj.clientY;
-
-    //     document.onmousemove = moveDrag;
-    //     document.onmouseup = stopDrag;
-    //     if (e_obj.preventDefault) e_obj.preventDefault();
-
-    // }
-
-    // function stopDrag() {
-    //     document.onmousemove = null;
-    //     document.onmouseup = null;
-    //     if (correct == 1) { //goodluck 정답
-    //         window.location.href = 'http://localhost:3000/finish';
-    //     }
-    //     else { //goodluck 정답 실패
-    //         window.location.href = 'http://localhost:3000/finalgameover';
-    //     }
-    // }
 
     const check = (sentence) => {
         const answer = ['g', 'o', 'o', 'd', 'l', 'u', 'c', 'k'];
@@ -59,16 +18,40 @@ function GamePage(props) {
     }
 
     const [correct, setCorrect] = useState(0);
+
     const inspect = () => {
         //정답을 맞추면
-        if (check(sentence) == 1)
+        console.log("check(sentence)" + check(sentence));
+        if (check(sentence) == 1) {
             setCorrect(1);
+        }
+        else {
+            setCorrect(0);
+        }
     }
 
     let sentence = [];
+
     function addWord(w) {
-        sentence.push(w);
+        console.log("Before Correct:" + correct);
+        if (sentence.length < 8)
+            sentence.push(w);
+        console.log(sentence);
         inspect(sentence);
+        console.log("Correct:" + correct);
+    }
+
+    const onDragEnd = () => {
+        console.log("드래그");
+        console.log("correct:" + correct);
+        console.log("sentence length:" + sentence.length);
+        if (correct === 0 && sentence.length >= 8)
+            window.location.href = 'http://localhost:3000/finalgameover';
+        else if (correct === 1 && sentence.length == 8) {
+            setCorrect(0);
+            window.location.href = 'http://localhost:3000/finish';
+        }
+
     }
 
     return (
@@ -76,10 +59,32 @@ function GamePage(props) {
             <Header />
             <img src='/img/game/background.png' className="background" alt="배경" />
             <img src='/img/game/window.png' className="keyWindow" alt="글자입력칸" />
-            <img src='/img/game/lockedDoor.png' className="door" alt="문" />
-            <img src='/img/game/key.png' className="key" onmousedown="startDrag(event, this)" />
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="OpenDoor">
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            <img src='/img/game/lockedDoor.png' className="door" alt="문" />
+                            <Draggable draggableId="UseKey" index="1">
+                                {(provided, snapshot) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}>
+                                        <img src='/img/game/key.png' className="key" />
+                                    </div>
+                                )}
+                            </Draggable>
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
             <img src='/img/game/guideText.png' className="guideText" alt="설명" />
             <img src='/img/game/gameCharacter.png' className="character" alt="주머니" />
+
             <div className="word1" value="g" onClick={() => addWord('g')}>g</div>
             <div className="word2" value="o" onClick={() => addWord('o')}>o</div>
             <div className="word3" value="o" onClick={() => addWord('o')}>o</div>
