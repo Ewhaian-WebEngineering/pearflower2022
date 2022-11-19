c138823ae85fac10940e019d5dcde24a52eba65a
YoonKyoungMin <yoongang02@gmail.com>
2022-11-20 05:28:08 +0900

    [cartoon] 캐릭터 소개 페이지와 각 캐릭터 페이지들 임의 연결

--- src/views/cartoon/CharacterInfo.js
+++ src/views/cartoon/CharacterInfo.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function CharacterInfo() {
   return (
     <div>
       <h1>Character Info Page</h1>
+      <Link to="/mushroom1">
+        <h2>Mushroom</h2>
+      </Link>
+      <Link to="/bee1">
+        <h2>Bee</h2>
+      </Link>
+      <Link to="/sprout1">
+        <h2>Sprout</h2>
+      </Link>
+      <Link to="/gardener1">
+        <h2>Gardener</h2>
+      </Link>
     </div>
   );
 }
