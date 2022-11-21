0e411097048f63efc994a4553dd9764b0a5d29cd
YoonKyoungMin <yoongang02@gmail.com>
2022-11-22 05:06:38 +0900

    [cartoon] 인트로 페이지

--- src/views/Intro.js
+++ src/views/Intro.js
@@ -1,22 +1,65 @@
-import React from "react";
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./Intro.css";
 
+var clickCnt;
+function setScreenSize() {
+  let vh = window.innerHeight;
+  let vw = window.innerWidth;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
+}
+
+const textSrcs = [
+  "/img/cartoon/intro/text1.png",
+  "/img/cartoon/intro/text2.png",
+  "/img/cartoon/intro/text3.png",
+  "/img/cartoon/intro/text4.png",
+  "/img/cartoon/intro/text5.png",
+  "/img/cartoon/intro/text6.png",
+  "/img/cartoon/intro/text7.png",
+  "/img/cartoon/intro/text8.png",
+  "/img/cartoon/intro/text9.png",
+];
 function Intro() {
+  useEffect(() => {
+    clickCnt = 1;
+    setScreenSize();
+    var target = document.getElementById("intro_speech_bubble_wrap");
+    target.style.backgroundImage = `url(/img/cartoon/intro/text1.png)`;
+  }, []);
+
   return (
-    <div>
-      카툰페이지 인트로입니다! <br />
-      <Link to="/characterInfo">
-        <button>캐릭터 소개페이지</button>
-      </Link>
-      <Link to="/main">
-        <button>메인페이지</button>
-      </Link>
-      <Link to="/event">
-        <button>이벤트페이지</button>
-      </Link>
-      <Link to="/goods">
-        <button>굿즈페이지</button>
-      </Link>
+    <div id="intro_container">
+      <div id="intro_ground"></div>
+      <div id="intro_black_cat_shadow_wrap">
+        <div id="intro_black_cat_shadow"></div>
+        <div id="intro_black_cat"></div>
+      </div>
+      <div id="intro_speech_bubble_wrap">
+        <div
+          id="intro_speech_bubble_btn"
+          onClick={() => {
+            if (clickCnt != 8) {
+              clickCnt++;
+              var target = document.getElementById("intro_speech_bubble_wrap");
+              target.style.backgroundImage = `url(${textSrcs[clickCnt]})`;
+            }
+          }}
+        ></div>
+      </div>
+      <div className="cartoon_btns inlineBlock">
+        <Link to="#">
+          <div className="cartoonPrevBtn inlineBlock cartoonBtn"></div>
+        </Link>
+        <Link to="/main">
+          <div className="cartoonSkipBtn inlineBlock cartoonBtn"></div>
+        </Link>
+        <Link to="/characterInfo">
+          <div className="cartoonNextBtn inlineBlock cartoonBtn"></div>
+        </Link>
+      </div>
     </div>
   );
 }
