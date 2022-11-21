b9c5127ad636ee1aa3ac3f1f85a84328cbeeaf8d
gaga <jcky0511@gmail.com>
2022-11-21 19:05:32 +0900

    다시 생각해보세요

--- src/views/game/GameMain.js
+++ src/views/game/GameMain.js
@@ -11,7 +11,6 @@ const questions = [
         index: 1,
         question: "이화이언이 시작된 해는?",
         answer: "2001",
-        len: 4,
         hint: "우리나라가 월드컵과 붉은 악마로 가득했던 시기 - 1",
         item: "g"
     },
@@ -19,14 +18,12 @@ const questions = [
         index: 2,
         question: "이화여자대학교 생협 메뉴 중 하나로, \n아이스티와 아메리카노를 섞은 인기 메뉴의 명칭은?",
         answer: "아이스티노",
-        len: 5,
         hint: "첫 번째 메뉴의 네 글자와 두 번째 메뉴의 한 글자를 적절히 섞어 보세요!",
         item: "o"
     }, {
         index: 3,
         question: "이화이언의 운영진 팀의 개수는?",
         answer: "3",
-        len: 3,
         hint: "21 나누기 7, 27 나누기 9의 공통점은?",
         item: "o"
     },
@@ -34,14 +31,12 @@ const questions = [
         index: 4,
         question: "이번 11월, 이화이언에서 개최하는 행사의 명칭은?",
         answer: "배꽃정원",
-        len: 4,
         hint: "이화여자대학교를 대표하는 꽃 + 꽃을 주로 키우는 장소 중 하나의 명칭",
         item: "d"
     }, {
         index: 5,
         question: "이화이언에서 비밀단어를 입장하면 접속 가능한 곳은?",
         answer: "비밀의화원",
-        len: 5,
         hint: "이화이언 사이트 우측 상단 메뉴의 '열쇠고리' 아이콘을 누르면 접속 가능한 곳은?",
         item: "l"
     },
@@ -49,7 +44,6 @@ const questions = [
         index: 6,
         question: "이화여자대학교를 대표하는 숫자 4자리",
         answer: "1886",
-        len: 4,
         hint: "이화학당이 창설된 해",
         item: "u"
     },
@@ -57,14 +51,12 @@ const questions = [
         index: 7,
         question: "이화여자대학교캠퍼스복합단지라고도 불리는\n 이화여자대학교의 대표 건축물 명칭은?",
         answer: "ECC",
-        len: 3,
         hint: "각 단어를 끊어 영어로 바꿔보고, 각 영단어의 첫 이니셜을 조합해보세요!",
         item: "c"
     }, {
         index: 8,
         question: "이화여자대학교에서 뽀미와 뽀삐를 볼 수 있는 장소는?",
         answer: "학생문화관",
-        len: 5,
         hint: "연습실부터 생협, 라운지, 동방까지 없는 게 없는 이 곳은 어디?",
         item: "k"
     }
@@ -76,24 +68,34 @@ function GameMain() {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [userAnswer, setUserAnswer] = useState('');
     const [popUp, setpopUp] = useState(false);
+    const [visible, setVisible] = useState(false);
 
     const navigate = useNavigate();
 
-    //해당 인덱스답과 사용자 입력 답이 맞을 시 , currentIndex+1
-    const checkAns = () => {
-        if (currentIndex < 7 && userAnswer === questions[currentIndex].answer) {
-            setCurrentIndex(currentIndex + 1)
-        } else if (currentIndex === 7 && userAnswer === questions[currentIndex].answer) {
-            navigate('/finalgame');
-        }
+    function wrong() {
 
     }
+
+    //해당 인덱스답과 사용자 입력 답이 맞을 시 , currentIndex+1
+    function checkAns() {
+        if (currentIndex < 7 && userAnswer === questions[currentIndex].answer) {
+            setVisible((visible) => false)
+            setCurrentIndex(currentIndex + 1)
+        } else if (currentIndex === 7 && userAnswer === questions[currentIndex].answer) {
+            setVisible((visible) => false)
+            navigate('/finalgame');
+        } else if (userAnswer !== questions[currentIndex].answer) { /*다시 생각해보세요*/
+            setVisible((visible) => true);
+        }
+    }
+
 
     // const onCheckEnter = (e) => {   /*enter키 눌러도 반환*/
     //     if (e.key === 'Enter') {
     //         checkAns();
     //     }
     // }
+
 
 
 
@@ -109,6 +111,9 @@ function GameMain() {
             <div className="gamemiddle">
                 <div className="main1">
                     <img className="_question" alt="question" src="/img/game/Question.png" />
+                    {
+                        visible && <img className="wrong" src="/img/game/wrong.png"></img>
+                    }
                     <span>
 
                         {/* <span className="popupHint">{questions[currentIndex].hint}</span> */}
