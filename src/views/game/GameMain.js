96897d1cd774e71ac9dac7773299d4cb70e5a713
hyeinNa <nhi3373@gmail.com>
2022-11-21 21:48:49 +0900

    엔터키 정답확인함수 추가

--- src/views/game/GameMain.js
+++ src/views/game/GameMain.js
@@ -12,14 +12,14 @@ const questions = [
         question: "이화이언이 시작된 해는?",
         answer: "2001",
         hint: "우리나라가 월드컵과 붉은 악마로 가득했던 시기 - 1",
-        item: "g"
+        item: "l"
     },
     {
         index: 2,
         question: "이화여자대학교 생협 메뉴 중 하나로, \n아이스티와 아메리카노를 섞은 인기 메뉴의 명칭은?",
         answer: "아이스티노",
         hint: "첫 번째 메뉴의 네 글자와 두 번째 메뉴의 한 글자를 적절히 섞어 보세요!",
-        item: "o"
+        item: "d"
     }, {
         index: 3,
         question: "이화이언의 운영진 팀의 개수는?",
@@ -32,13 +32,13 @@ const questions = [
         question: "이번 11월, 이화이언에서 개최하는 행사의 명칭은?",
         answer: "배꽃정원",
         hint: "이화여자대학교를 대표하는 꽃 + 꽃을 주로 키우는 장소 중 하나의 명칭",
-        item: "d"
+        item: "k"
     }, {
         index: 5,
         question: "이화이언에서 비밀단어를 입장하면 접속 가능한 곳은?",
         answer: "비밀의화원",
         hint: "이화이언 사이트 우측 상단 메뉴의 '열쇠고리' 아이콘을 누르면 접속 가능한 곳은?",
-        item: "l"
+        item: "g"
     },
     {
         index: 6,
@@ -58,7 +58,7 @@ const questions = [
         question: "이화여자대학교에서 뽀미와 뽀삐를 볼 수 있는 장소는?",
         answer: "학생문화관",
         hint: "연습실부터 생협, 라운지, 동방까지 없는 게 없는 이 곳은 어디?",
-        item: "k"
+        item: "o"
     }
 ]
 
@@ -66,15 +66,19 @@ const questions = [
 function GameMain() {
 
     const [currentIndex, setCurrentIndex] = useState(0);
+    // const [currentItem, setCurrentItem] = useState(0);
     const [userAnswer, setUserAnswer] = useState('');
     const [popUp, setpopUp] = useState(false);
     const [visible, setVisible] = useState(false);
 
     const navigate = useNavigate();
 
-    function wrong() {
-
+    const items = []
+    items.push("")
+    for (var ele of questions) {
+        items.push(ele.item)
     }
+
 
     //해당 인덱스답과 사용자 입력 답이 맞을 시 , currentIndex+1
     function checkAns() {
@@ -90,12 +94,15 @@ function GameMain() {
     }
 
 
-    // const onCheckEnter = (e) => {   /*enter키 눌러도 반환*/
-    //     if (e.key === 'Enter') {
-    //         checkAns();
-    //     }
-    // }
-
+    const handleOnClick = () => {
+        checkAns();
+    };
+    const handleOnKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleOnClick(); // Enter 입력이 되면 클릭 이벤트 실행
+            e.preventDefault();//새로고침방지
+        }
+    };
 
 
 
@@ -132,16 +139,18 @@ function GameMain() {
                         <img id="result_rec" alt="result" src="/img/game/AnswerRec.png" ></img>
 
                         {/* 사용자가 답을 입력함 */}
-                        <form >
-                            <input type="text" onChange={(e) => setUserAnswer(e.target.value)} ></input>
+                        <form>
+                            <input type="text" onChange={(e) => setUserAnswer(e.target.value)} onKeyPress={handleOnKeyPress} ></input>
+                            <input type="hidden" ></input>
                             <div className="answerBtn" >
-                                <img alt="answer" id="_answerBtn" src="/img/game/answerBtn.png" type="submit" onClick={checkAns}
+                                <img alt="answer" id="_answerBtn" src="/img/game/answerBtn.png" type="submit" onClick={handleOnClick}
                                 />
                             </div>
                         </form>
 
                     </div>
                     <div className="pocket" >
+                        <span className='goodluck'></span>
                         <img className="_pocket" alt="pocket" src="/img/game/mainPocket.png" />
                     </div>
                 </div>
