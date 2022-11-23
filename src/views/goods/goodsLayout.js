8995096257185d28c557aa7ec328e5ccf045e90e
binggamel <kcyeun1203@naver.com>
2022-11-23 10:35:27 +0900

    goodsPage mobile

--- src/views/goods/goodsLayout.js
+++ src/views/goods/goodsLayout.js
@@ -27,9 +27,16 @@ function goodsLayout() {
         "../img/goods/goods4.png",
         "../img/goods/goods5.png",
         "../img/goods/goods6.png",
-        
     ]
 
+    const imgTitle = [
+        { name: "스티커팩", price: 3000 },
+        { name: "유리컵", price:3000},
+        { name: "말랑비즈인형", price:6000},
+        { name: "멀티클리너", price:3000},
+        { name: "레고피규어", price:9000},
+        { name: "그립톡", price:3000},
+    ]
 
     return (
         <div>
@@ -38,7 +45,15 @@ function goodsLayout() {
                 {/* test */}
                 <div className='goodsArray'>
                     {new Array(6).fill(null).map((_, index) => (
-                        <img src={imgSrc[index]} alt="" onClick={()=>ActiveLink(index)}/>
+                        <>
+                            <div className='goodsArrayBox'>
+                                <img src={imgSrc[index]} alt="" onClick={() => ActiveLink(index)} />
+                                <div className='goodsData'>
+                                    <p>{imgTitle[index].name}</p>
+                                    <p>{imgTitle[index].price}</p>
+                                </div>
+                            </div>
+                        </>
                     ))}
                     
                 </div>
