4fb067d0e1e026ec03d21ee4ccf6e988f7ce1141
gaga <jcky0511@gmail.com>
2022-11-24 01:53:19 +0900

    헤더 변경

--- src/views/game/GameFinish.js
+++ src/views/game/GameFinish.js
@@ -3,13 +3,23 @@ import { Link } from 'react-router-dom'
 import Header from './WebGameHeader'
 import './GamePage.css'
 import './GameInfo.css'
+import './Gamehome.css'
 
 
 function GameFinish(props) {
 
     return (
         <div >
-            <Header />
+            <div className='gameheader'>
+                <div className='header-box'>
+                    <Link to='/main'>
+                        <img id='logo' src='img/logo_main.png' alt='logo main'></img>
+                    </Link>
+                    <Link to="/main">
+                        <img src='img/game/exit.png' className="backBtn" alt="나가기" />
+                    </Link>
+                </div>
+            </div>
             <img src='img/game/background.png' className="background" alt="배경" />
             <img src='img/game/openDoor.png' className="open" alt="성공" />
             <span className='howtolabel'>와!! 탈출성공</span>
